ff380f082044f79e7af02c1936347654c482d3c9
zach401 <zach401@gmail.com>
2020-10-05 11:19:06 -0700

    By default use JupyterLab default port 8888.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -p 8890:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work \
+docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work \
 jupyter/scipy-notebook:latest
