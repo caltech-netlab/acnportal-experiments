e9c8a1253cd419bf46da103f29c6bffd5ba49973
zach401 <zach401@gmail.com>
2021-01-02 11:58:29 -0500

    Update docker file and run script.

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -p 8890:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work acn-sim-demo:latest
+docker build -t acnportal_experiments:latest .
+docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work acnportal_experiments:latest
